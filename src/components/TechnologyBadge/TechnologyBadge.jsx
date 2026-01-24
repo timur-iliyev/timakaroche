097cfe8d48b5ad2b6dd99165fcb53412a10c15a9
Timur Iliyev <timur.dela@gmail.com>
2026-01-24 13:24:10 +0100

    feat: add TechnologyBadge component with related assets

---
+++ src/components/TechnologyBadge/TechnologyBadge.jsx
@@ -0,0 +1,24 @@
+import './TechnologyBadge.scss'
+import clsx from 'clsx'
+
+export default (props) => {
+  const { className, technologyName, logoName } = props
+
+  return (
+    <figure className={clsx(className, 'technology-badge')}>
+      <div className="technology-badge__body">
+        <img
+          src={
+            `/src/assets/images/technologyLogos/${logoName}` + '.svg'
+          }
+          alt={technologyName}
+          className="technology-badge__logo"
+          title={technologyName}
+        />
+      </div>
+      <figcaption className="technology-badge__caption">
+        {technologyName}
+      </figcaption>
+    </figure>
+  )
+}
